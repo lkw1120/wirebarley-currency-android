09687ae4d11fa80c05c72a1db34da2d8525af114
lkw1120 <lkwkang@gmail.com>
2022-12-12 22:59:52 +0900

    API 키를 local.properties 에서 읽어오도록 수정

--- app/src/main/java/com/lkw1120/currency/common/Constants.kt
+++ app/src/main/java/com/lkw1120/currency/common/Constants.kt
@@ -1,7 +1,9 @@
 package com.lkw1120.currency.common
 
+import com.lkw1120.currency.BuildConfig
+
 
 const val DATABASE_NAME = "currency-db"
 const val COUNTRY_INFO_FILENAME = "country_info.json"
 
-const val API_KEY = "7U61n6w2uU60gyyK0XYHzREWphaWGzbW"
+const val API_KEY = BuildConfig.API_KEY
